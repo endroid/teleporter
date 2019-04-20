7dfc540b3a4d769d81269b9ae7cc533a3a10e1b4
Jeroen van den Enden <info@endroid.nl>
2019-04-20 21:04:14 +0000

    Create test

---
+++ tests/Command/TeleportCommandTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Jeroen van den Enden <info@endroid.nl>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Endroid\Teleporter\Command;
+
+use Endroid\Teleporter\Teleporter;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class TeleportCommandTest extends TestCase
+{
+    public function testExecute()
+    {
+        $teleporter = new Teleporter();
+        $teleportCommand = new TeleportCommand($teleporter);
+        $commandTester = new CommandTester($teleportCommand);
+        $commandTester->execute([
+            'sourcePath' => __DIR__.'/../source',
+            'targetPath' => __DIR__.'/../target',
+        ]);
+
+        $this->assertEquals('interact called'.PHP_EOL.'execute called'.PHP_EOL, $tester->getDisplay(), '->run() calls the interact() method if the input is interactive');
+    }
+}
